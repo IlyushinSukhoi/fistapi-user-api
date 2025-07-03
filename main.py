b26580e9a5aa9ec20a98b138396dcb9217e3b32c
IlyushinSukhoi <mayu012921@gmail.com>
2025-07-04 02:51:48 +0900

    fix

--- main.py
+++ main.py
@@ -167,5 +167,7 @@ async def close_account(authenticated_user: str = Depends(authenticate_user)):
 
 if __name__ == "__main__":
     import uvicorn
+    import os
     
-    pass
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
